c060b4bd6b96361a803038016e5fdc7389d2b3c1
Gustavo Santos <gustavosms@localhost.localdomain>
2018-12-20 23:28:21 -0200

    creating method for couting sequences

--- src/main/java/com/meli/mutantes/DNAVerifier.java
+++ src/main/java/com/meli/mutantes/DNAVerifier.java
@@ -7,4 +7,25 @@ public class DNAVerifier {
         return true;
     }
 
+    private int countSequences(String line, int amount) {
+        char[] chars = line.toCharArray();
+        int sequencesFound = 0;
+        char computedChar = chars[0];
+        int computedAmount = 1;
+        for(int i = 1; i < chars.length; i++) {
+            if (chars[i] == computedChar) {
+                if (computedAmount == amount - 1) {
+                    sequencesFound++;
+                    computedAmount = 1;
+                } else {
+                    computedAmount++;
+                }
+            } else {
+                computedChar = chars[i];
+                computedAmount = 1;
+            }
+        }
+        return sequencesFound;
+    }
+
 }
