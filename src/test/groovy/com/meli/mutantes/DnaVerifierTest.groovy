26bdd94319d4045d4e217a72265841f30d8ee740
Gustavo Santos <gustavosms@localhost.localdomain>
2018-12-24 16:47:19 -0200

    including more scenarios

--- src/test/groovy/com/meli/mutantes/DnaVerifierTest.groovy
+++ src/test/groovy/com/meli/mutantes/DnaVerifierTest.groovy
@@ -35,6 +35,90 @@ class DnaVerifierTest extends Specification {
         !dnaVerifier.isMutant(getDnaArray(dna))
     }
 
+    @Test
+    void "Mutant DNA in verticals"() {
+        given:
+        def dna = [ ['A', 'T', 'G', 'C', 'G', 'A'],
+                    ['A', 'A', 'G', 'T', 'G', 'C'],
+                    ['A', 'T', 'C', 'T', 'G', 'T'],
+                    ['A', 'G', 'A', 'A', 'G', 'G'],
+                    ['C', 'C', 'T', 'C', 'T', 'A'],
+                    ['T', 'C', 'A', 'C', 'T', 'G'] ]
+
+        expect:
+        dnaVerifier.isMutant(getDnaArray(dna))
+    }
+
+    @Test
+    void "Single sequence in vertical dont characterize a mutant"() {
+        given:
+        def dna = [ ['A', 'T', 'G', 'C', 'G', 'A'],
+                    ['A', 'A', 'G', 'T', 'T', 'C'],
+                    ['A', 'T', 'C', 'T', 'G', 'T'],
+                    ['A', 'G', 'A', 'A', 'G', 'G'],
+                    ['C', 'C', 'T', 'C', 'T', 'A'],
+                    ['T', 'C', 'A', 'C', 'T', 'G'] ]
+
+        expect:
+        !dnaVerifier.isMutant(getDnaArray(dna))
+    }
+
+    @Test
+    void "Mutant DNA in main diagonal"() {
+        given:
+        def dna = [ ['A', 'C', 'G', 'C', 'G', 'A'],
+                    ['C', 'A', 'T', 'T', 'G', 'C'],
+                    ['T', 'T', 'A', 'T', 'C', 'T'],
+                    ['A', 'G', 'A', 'A', 'T', 'G'],
+                    ['C', 'A', 'T', 'C', 'T', 'T'],
+                    ['T', 'C', 'A', 'C', 'T', 'G'] ]
+
+        expect:
+        dnaVerifier.isMutant(getDnaArray(dna))
+    }
+
+    @Test
+    void "Single sequence in main diagonal dont characterize a mutant"() {
+        given:
+        def dna = [ ['A', 'C', 'G', 'C', 'G', 'A'],
+                    ['C', 'A', 'T', 'T', 'G', 'C'],
+                    ['T', 'T', 'C', 'T', 'C', 'T'],
+                    ['A', 'G', 'A', 'A', 'T', 'G'],
+                    ['C', 'A', 'T', 'C', 'T', 'T'],
+                    ['T', 'C', 'A', 'C', 'T', 'G'] ]
+
+        expect:
+        !dnaVerifier.isMutant(getDnaArray(dna))
+    }
+
+    @Test
+    void "Mutant DNA in secondary diagonal"() {
+        given:
+        def dna = [ ['A', 'C', 'G', 'T', 'G', 'A'],
+                    ['C', 'G', 'T', 'G', 'G', 'C'],
+                    ['T', 'A', 'G', 'T', 'C', 'T'],
+                    ['T', 'G', 'A', 'A', 'T', 'G'],
+                    ['C', 'A', 'T', 'C', 'T', 'T'],
+                    ['T', 'C', 'A', 'C', 'T', 'G'] ]
+
+        expect:
+        dnaVerifier.isMutant(getDnaArray(dna))
+    }
+
+    @Test
+    void "Single sequence in secondary diagonal dont characterize a mutant"() {
+        given:
+        def dna = [ ['A', 'C', 'G', 'T', 'G', 'A'],
+                    ['C', 'G', 'T', 'G', 'G', 'C'],
+                    ['T', 'A', 'C', 'T', 'C', 'T'],
+                    ['T', 'G', 'A', 'A', 'T', 'G'],
+                    ['C', 'A', 'T', 'C', 'T', 'T'],
+                    ['T', 'C', 'A', 'C', 'T', 'G'] ]
+
+        expect:
+        !dnaVerifier.isMutant(getDnaArray(dna))
+    }
+
     static String[] getDnaArray(List<List<String>> dna) {
         dna.collect { it.join("") }.toArray(new String[0])
     }
